0683e6bfea8b67abb77cc1c9e35a74b766aa95b4
PR1YAM9 <priyam9maini@gmail.com>
2023-07-06 22:00:00 +0530

    SEEEEEESSSHHHHH

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import ChatBot from 'react-simple-chatbot'
+import { Segment,Button } from 'semantic-ui-react'
 
-function App() {
+const App = () => {
+
+  const [showChatbot, setShowChatbot] = useState(false);
+  const toggleChatbot = () => {
+    setShowChatbot(!showChatbot);
+  };
+
+  const steps = [
+    {
+      id: 'Greet',
+      message: 'Hello, Welcome to our website',
+      trigger: 'Ask Name'
+    },
+    {
+      id: 'Ask Name',
+      message: 'Please type your name?',
+      trigger: 'GetName'
+    },
+    {
+      id: 'GetName',
+      user: true,
+      trigger: 'ShowName'
+    },
+    {
+      id: 'ShowName',
+      message: 'Hi {previousValue}, please select an option',
+      trigger: 'ShowOptions'
+    },
+    {
+      id: 'ShowOptions',
+      options: [
+        { value: 'Book an appointment', label: 'Book an appointment', trigger: 'Book' },
+        { value: 'Cancel an appointment', label: 'Cancel an appointment', trigger: 'Cancel' }
+      ]
+    },
+    {
+      id: 'Book',
+      message: 'Please select the doctor',
+      end: true
+    },
+    {
+      id: 'Cancel',
+      message: 'Please select the doctor',
+      end: true
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Segment floated="right">
+    {showChatbot ? (
+      <ChatBot steps={steps} />
+    ) : (
+      <Button onClick={toggleChatbot}>Open Chatbot</Button>
+    )}
+  </Segment>
+  )
 }
 
-export default App;
+export default App
